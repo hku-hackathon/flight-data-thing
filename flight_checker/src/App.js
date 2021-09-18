a1b8ff1b3cf2f1300c81d1181de88f1caeb46188
skywcs <skywcs@connect.hku.hk>
2021-09-18 17:18:27 +0800

    Added title

--- flight_checker/src/App.js
+++ flight_checker/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import { Button } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import Title from "./Title/Title"
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <Button>Testing</Button> 
     </div>
   );
