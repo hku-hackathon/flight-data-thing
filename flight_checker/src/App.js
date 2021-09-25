a90890d7a0fefbb8de18ac52f2a470fccd82cc2d
skywcs <skywcs@connect.hku.hk>
2021-09-25 15:54:32 +0800

    WIP fetch and display flight data
    Co-authored-by: kan <kan2k@users.noreply.github.com>
    Co-authored-by: Lemuel <lemuellee.kl@gmail.com>

--- flight_checker/src/App.js
+++ flight_checker/src/App.js
@@ -1,8 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import Title from "./Title/Title"
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
 
 function App() {
+  const [flightDataList, setFlightDataList] = useState([]);
+  const [flightData, setFlightData] = useState({ destination : "" , flightNumber : ""});
+  const { destination, flightNumber } = flightData;
+  async function fetchFlightDataList() {
+    const { data } = await supabase.from('flight_data').select()
+    setFlightDataList(data)
+    console.log(flightDataList)
+  }
+
+  useEffect(() => {
+    fetchFlightDataList()
+  },[])
+
   return (
     <div className="App">
       <Title />
