ea570bb51a58d22b85e2b04b2c4d862449c9e360
skywcs <skywcs@connect.hku.hk>
2021-10-02 15:17:54 +0800

    formatting with saving

--- flight_checker/src/App.js
+++ flight_checker/src/App.js
@@ -1,29 +1,31 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
-import Title from "./Title/Title"
-import { useState, useEffect } from 'react';
-import { supabase } from './supabaseClient';
+import Title from "./Title/Title";
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
 
 function App() {
-  const [flightDataList, setFlightDataList] = useState([]);
-  const [flightData, setFlightData] = useState({ destination : "" , flightNumber : ""});
-  const { destination, flightNumber } = flightData;
-  async function fetchFlightDataList() {
-    const { data } = await supabase.from('flight_data').select()
-    setFlightDataList(data)
-    console.log(flightDataList)
-  }
+    const [flightDataList, setFlightDataList] = useState([]);
+    const [flightData, setFlightData] = useState({
+        destination: "",
+        flightNumber: "",
+    });
+    const { destination, flightNumber } = flightData;
+    async function fetchFlightDataList() {
+        const { data, error } = await supabase.from("flight_data").select();
+        setFlightDataList(data);
+        console.log(flightDataList);
+    }
+    useEffect(() => {
+        fetchFlightDataList();
+    }, []);
 
-  useEffect(() => {
-    fetchFlightDataList()
-  },[])
-
-  return (
-    <div className="App">
-      <Title />
-      <Button>Testing</Button> 
-    </div>
-  );
+    return (
+        <div className="App">
+            <Title />
+            <Button>Testing</Button>
+        </div>
+    );
 }
 
 export default App;
